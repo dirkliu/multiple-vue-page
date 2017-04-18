0caa86867abbd4893a48d8430dd438c01c866235
dirkliu <liuqi0702@126.com>
2017-04-18 20:29:11 +0800

    test vue-json-editor

--- views/activity/App.vue
+++ views/activity/App.vue
@@ -4,11 +4,23 @@
     <header>
       <a href="index.html">首页</a>
     </header>
+    <vue-json-editor v-model="json"></vue-json-editor>
   </div>
 </template>
 
 <script>
+import vueJsonEditor from 'vue-json-editor'
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      json: {
+        name: 'liu77'
+      }
+    }
+  },
+  components: {
+    vueJsonEditor
+  }
 }
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
     }, {
       test: /.vue$/,
       loader: "vue-loader"
+    }, {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'img/[name].[hash:7].[ext]'
+        }
     }]
   },
 
